a36b8d15708db32c5081d15bb929d29cc57db2e9
Md. Masud Iqbal <iqbaldiit@gmail.com>
2019-11-08 20:27:11 +0600

    Add files via upload

---
+++ Population Density Difference.sql
@@ -0,0 +1,22 @@
+/*
+Population Density Difference
+=================================
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+*/
+
+DECLARE @tbl_CITY AS TABLE (ID int, [Name] varchar(17),CountryCode varchar(3),District varchar(20),[Population] int)
+INSERT INTO @tbl_CITY VALUES (1,'CITY1','001','DIS1',100000)
+INSERT INTO @tbl_CITY VALUES (2,'CITY2','JPN','California',200000)
+INSERT INTO @tbl_CITY VALUES (3,'CITY3','JPN','DIS3',300000)
+INSERT INTO @tbl_CITY VALUES (4,'CITY4','004','California',400000)
+INSERT INTO @tbl_CITY VALUES (5,'CITY5','JPN','DIS5',500000)
+
+SELECT MAX([Population])-MIN([Population]) AS Pop FROM @tbl_CITY
+
+/*
+Hacker Rank Submission
+----------------------
+SELECT MAX([Population])-MIN([Population]) AS Pop FROM CITY;
+
+*/
